5c230e26a8967c1f6c2adcbd1ba3e09b1a66507b
Ibrahim-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-07-07 19:17:19 +0200

    bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import { join } from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config';
 
 class Server {
-	constructor(port, app) {
+	constructor(port, app,compiler) {
 		this.port = port;
 		this.app = app;
+		this.compiler = compiler;
 	}
 	core() {
+		this.app.use(require('webpack-dev-middleware')(this.compiler,{ noInfo: true, publicPath: config.output.publicPath}));
 		this.app.get('/', (req, res)=>{res.sendFile(join(__dirname, '../src/index.html'));});
 		this.app.listen(this.port,() =>{ open(`http://localhost:${this.port}`);});
 	}
 }
 
-let server = new Server(3000, express());
+let server = new Server(3000, express(), webpack(config));
 server.core();
